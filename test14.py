c361dfdaa22853acccb0e760f81c2dd18b12944a
DinhNguyen-vj <dinhxuannguyen09@gmail.com>
2024-08-01 09:07:54 +0700

    Update test14.py

--- test14.py
+++ test14.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import plotly.graph_objects as go
+import st.plotly_graph_objects as go
 from collections import deque
 
 # Custom CSS to adjust the width of the Streamlit app
